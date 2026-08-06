7593228dacb280b5027aef56f2fb039e13ae912c
Sahib Nanda <essenbeats@gmail.com>
2026-08-06 06:19:54 +0530

    check: fake test

---
+++ src/test/java/net/sahibnanda/portfolio/abc/FakeFailingTest.java
@@ -0,0 +1,13 @@
+package net.sahibnanda.portfolio.abc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FakeFailingTest {
+
+    @Test
+    void failTest() {assertEquals(1, 2, "1 != 2");}
+
+
+}
